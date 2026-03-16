89548830b17c610c2b260974d191f9180f06cffd
JoeAustein <joe.austein@gmail.com>
2026-03-16 19:00:27 -0300

    Implementação Churrascometro

--- app/src/main/java/com/example/kotlin04_churrascometro/MainActivity.kt
+++ app/src/main/java/com/example/kotlin04_churrascometro/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.kotlin04_churrascometro
 
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        // Implementacoes:
+        var sbMen = findViewById<SeekBar>(R.id.SeekBarMen)
+        var menQtt = findViewById<TextView>(R.id.TextViewMen)
+        var sbWomen = findViewById<SeekBar>(R.id.SeekBarWomen)
+        var womenQtt = findViewById<TextView>(R.id.TextViewWomen)
+
+        sbMen?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                menQtt.text = progress.toString()
+                calculate(progress, sbWomen.progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+
+        sbWomen?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                womenQtt.text = progress.toString()
+                calculate(sbMen.progress, progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+    }
+
+    fun calculate(menQtt: Int, womenQtt: Int) {
+        val outputSausage = findViewById<TextView>(R.id.TextViewQttSausage)
+        val outputMeat = findViewById<TextView>(R.id.TextViewQttMeat)
+        val sausageQtt: Double = (menQtt * 250.0 + womenQtt * 150.0)/1000
+        val meatQtt: Double = (menQtt * 450.0 + womenQtt * 300.0)/1000
+
+        outputSausage.text = sausageQtt.toString() + "Kg"
+        outputMeat.text = meatQtt.toString() + "Kg"
     }
 }
